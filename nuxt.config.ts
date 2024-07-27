82cf700aa1083f07791c7128702d9e7bb55a15e1
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-07-27 19:40:21 -0400

    build: update to package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
-    modules: ["../my-module/src/module"],
+    modules: ["nuxt-class-inject"],
     classInject: {
         storageKey: "nuxt-class-inject",
         globalName: "__NUXT_CLASS_INJECT__",
